a4ba9c596db8fed29e8d3a999f14bc80210ea8cc
NandiniNayak <nandini.r.nayak@gmail.com>
2024-12-04 01:24:35 +0000

    add model and table

---
+++ app/models/local_business.rb
@@ -0,0 +1,2 @@
+class LocalBusiness < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_140054) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_04_012338) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_140054) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "local_businesses", force: :cascade do |t|
+    t.string "business_id"
+    t.string "name"
+    t.string "types"
+    t.string "national_phone_number"
+    t.string "fomatted_address"
+    t.string "latitude"
+    t.string "longitude"
+    t.decimal "rating"
+    t.string "buiness_status"
+    t.json "json_data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
